d3b161b1c1088782b16899a80781a9702f0972a0
Sepideh Attar <sepideh.att@gmail.com>
2022-08-04 21:17:36 +0200

    Created sidbar Menu

---
+++ src/components/SideBarMenu.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import BSLogo from "./../assets/Logo.svg"
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import AddToQueueIcon from '@mui/icons-material/AddToQueue';
+import Home from '@mui/icons-material/Home';
+import { NavLink } from 'react-router-dom';
+
+
+
+const Container = styled.div`
+  flex: 1.2;
+  position: sticky;
+  top:0;
+  height:100vh;
+  background-color: #000;
+  color: white;
+`
+
+const ItemWrapper = styled.div`
+  padding: 20px 25px;
+`
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap:30px;
+  margin-bottom: 30px;
+`
+
+const Image = styled.img`
+  width:150px;
+  margin-bottom: 30px;
+`
+
+const Hr = styled.hr`
+opacity:0.4;
+  margin: 25px 2px;
+`
+
+
+const SideBarMenu = () => {
+  return (
+    <Container>
+      <ItemWrapper>
+        <NavLink to="/" style={{ textDecoration: "none" }}>
+          <Image src={BSLogo} />
+        </NavLink>
+          <NavLink to="/" style={{ textDecoration: "none", color:"inherit"}}>
+        <Item>
+            <Home />
+            Home
+        </Item>
+          </NavLink>
+        <Item>
+          <ThumbUpIcon />
+          Favorites
+        </Item>
+        <Item>
+          <AddToQueueIcon />
+          Subscription
+        </Item>
+        <Hr />
+        <Item>
+          SUBSCRIPTION LIST
+        </Item>
+        <p>Ivo Silva</p>
+        <p>Ivo Silva</p>
+        <p>Ivo Silva</p>
+        <Hr />
+        <Item>
+          FAVORITE VIDEOS
+        </Item>
+        <p>Video Title 1</p>
+        <p>Video Title 1</p>
+        <p>Video Title 1</p>
+      </ItemWrapper>
+    </Container>
+  )
+}
+
+export default SideBarMenu
