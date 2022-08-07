de89672be976c89fab4e5345a878fee321530d64
Sepideh Attar <sepideh.att@gmail.com>
2022-08-07 13:14:42 +0200

    created login page, completes styles

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,42 +1,121 @@
-import { useEffect, useState } from 'react';
-import jwt_decode from 'jwt-decode';
-const LogIn = () => {
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 
-  const [userDet, setUserDet] = useState({})
+const Container = styled.div`
+display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: calc(100vh - 56px);
+  
+`;
 
-  const handleCBResponse = (res) => {
-    const tokenDetails = jwt_decode(res.credential)
-    setUserDet(tokenDetails)
+
+const SubTitle = styled.h2`
+  font-size: 20px;
+  color: #af7b0c;
+
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 30%;
+  border-radius: 20px;
+  padding: 30px 80px;
+  gap: 10px;
+`;
+
+const Input = styled.input`
+border:none;
+ border-bottom: 1px solid gray;
+ outline:none;
+  border-radius: 20px;
+  padding: 10px;
+  color:white;
+  background-color: transparent;
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const LoginBtn = styled.button`
+  border-radius: 3px;
+  border: 1px solid #af7b0c;
+  color: #af7b0c;
+  padding: 10px 20px;
+  font-weight: 600;
+  cursor: pointer;
+  background-color:transparent;
+
+  :hover{
+    background-color: #af7b0c;
+    color:white;
   }
 
 
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id: "262976069051-4ge66qpfkhsrk5fg9rrpviljn03pg5qh.apps.googleusercontent.com",
-      callback: handleCBResponse
-    })
+`;
 
-    google.accounts.id.renderButton(
-      document.getElementById("sign-in"),
-      { theme: "outline", size: "large" }
-    )
-  }, [])
+const Title = styled.h1`
+font-size: 24px;
+`;
 
-console.log(userDet)
+
+
+const LogIn = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(undefined);
+
+  const navigate = useNavigate();
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+
+    //if username or password field is empty, return error message
+    if (email === "" || password === "") {
+      setErrorMessage((prevState) => ({
+        value: "Email/password field is empty",
+      }));
+
+    } else if(email && password ) {
+      //Signin Success
+      localStorage.setItem("isAuthenticated", "true");
+      navigate('/')
+    } 
+  };
+
 
 
   return (
-    <div>
-      <div id='sign-in'></div>
-      {userDet &&
-      <>
-      <h2>Hello {userDet.name}</h2>
-      <img src={userDet.picture} alt={userDet.name}/>
-      </>
-      }
-    </div>
+    <Container>
+        <Title>Sign In</Title>
+        <SubTitle>Welcome Here</SubTitle>
+        {errorMessage && <p>{errorMessage}</p>}
+        <Form onSubmit={handleLoginSubmit}>
+              <Input
+                placeholder="Email ..."
+                type="email"
+                name="email"
+                value={email}
+                required={true}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                placeholder="Password ..."
+                type="password"
+                name="password"
+                value={password}
+                required={true}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+          <LoginBtn type="submit">Sign in</LoginBtn>
+        </Form>
+    </Container>
   )
 }
 
