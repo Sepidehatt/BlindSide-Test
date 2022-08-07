658ffa499df90fadf10dfded959da57cb44dcbd2
Sepideh Attar <sepideh.att@gmail.com>
2022-08-07 13:09:01 +0200

    added dynamic data, like functionality,some style

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -6,19 +6,16 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import Comments from './Comments';
 import VideoCard from './VideoCard';
+import { useState} from 'react';
 
 const Container = styled.div`
 display: flex;
 flex-direction:column;
   gap: 24px;
-  height:100vh;
-
 `;
 
 const TopPart = styled.div`
 display: flex;
-  `
-const BottomPart = styled.div`
   `
 
 const VideoWrapper = styled.div`
@@ -27,11 +24,12 @@ const VideoWrapper = styled.div`
 `
 
 const Content = styled.div`
-  flex: 5;
+  flex: 4.5;
   `
 
 const Related = styled.div`
-  flex: 3;
+  flex: 3.5;
+  margin: 40px;
 `
 const VideoTitle = styled.h2`
 color:white;
@@ -46,23 +44,23 @@ margin: 0;
 color:lightgray;
 `
 const InfoWrapper = styled.div`
-border: 1px solid white;
+ border: 1px solid white;
  margin: 30px 0;
  padding: 10px;
-width:60%;
-display: flex;
-  justify-content: space-between;
+ width:60%;
+ display: flex;
+ justify-content: space-between;
+
 & > div{
   display: flex;
-align-items: center;
-gap:20px;
-
+  align-items: center;
+  gap:20px;
 }
 `
 
 const ProfileImage = styled.img`
   width:40px;
- border-radius:50%;
+  border-radius:50%;
 `
 const DetailWrapper = styled.div`
 width: 75%;
@@ -80,53 +78,78 @@ const IconBtnDiv = styled.div`
 display: flex;
 `
 const IconBtn = styled.div`
+margin-left: 10px;
+cursor:pointer;
 `
 const SubscribeBtn = styled.button`
+  border-radius: 3px;
+  border: 1px solid #af7b0c;
+  color: #af7b0c;
+  padding: 10px 20px;
+  font-weight: 600;
+  cursor: pointer;
+  background-color:transparent;
 
+  :hover{
+    background-color: #af7b0c;
+    color:white;
+  }
 `;
 
 const VideoDetails = () => {
+const [like, setLike] = useState('white')
+const [isLiked, setIsLiked] = useState(true)
+
   const { videoId } = useParams()
   const videoDetail = videos.find(video => videoId === video.id)
+  const relatedVideos = videos.filter(video => video.tag === videoDetail.tag && video.id !== videoDetail.id)
+
+  const likeHandler = ()=>{
+    setLike(isLiked ? "crimson" : "white");
+    setIsLiked(!isLiked)
+  }
+
+
   return (
     <Container>
       <TopPart>
         <Content>
-          <VideoWrapper>
-            <ReactPlayer controls url="https://vimeo.com/321921012" />
-          </VideoWrapper>
-          <DetailWrapper>
-            <div>
-              <VideoTitle>Video Title</VideoTitle>
-              <IconBtnDiv>
-                <IconBtn>
-                  <FavoriteIcon />
-                </IconBtn>
-                <IconBtn>
-                  <ShareIcon />
-                </IconBtn>
-              </IconBtnDiv>
-            </div>
-            <VideoText>Lorem ipsum, dolor sit amet consectetur adipisicing.</VideoText>
-            <InfoWrapper>
-            <div>
-              <ProfileImage src="https://robohash.org/quomaximeiste.jpg?size=70x70&set=set1" />
-              <Title>Username</Title>
-            </div>
-            <SubscribeBtn>Subscribe</SubscribeBtn>
-          </InfoWrapper>
-          </DetailWrapper>
-          <Comments/>
+          {videoDetail &&
+            <>
+              <VideoWrapper>
+                <ReactPlayer controls url={videoDetail.videoUrl} />
+              </VideoWrapper>
+              <DetailWrapper>
+                <div>
+                  <VideoTitle>{videoDetail.title}</VideoTitle>
+                  <IconBtnDiv>
+                    <IconBtn>
+                      <FavoriteIcon onClick={likeHandler} htmlColor={like} />
+                    </IconBtn>
+                    <IconBtn>
+                      <ShareIcon />
+                    </IconBtn>
+                  </IconBtnDiv>
+                </div>
+                <VideoText>Lorem ipsum, dolor sit amet consectetur adipisicing.</VideoText>
+                <InfoWrapper>
+                  <div>
+                    <ProfileImage src={videoDetail.profileImage} />
+                    <Title>{videoDetail.creator}</Title>
+                  </div>
+                  <SubscribeBtn>Subscribe</SubscribeBtn>
+                </InfoWrapper>
+              </DetailWrapper>
+            </>
+          }
+          <Comments />
         </Content>
         <Related>
-        <VideoCard/>
-        <VideoCard/>
-        <VideoCard/>
+          {relatedVideos &&
+            relatedVideos.map(video => <VideoCard type="related" videoDet={video} />)
+          }
         </Related>
       </TopPart>
-      <BottomPart>
-
-      </BottomPart>
     </Container>
   )
 }
